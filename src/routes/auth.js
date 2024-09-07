05be62126f1771cda0a9dfd6f6a9264d3387479f
harshitbhardwaj-09 <harshitbhardwaj99999@gmail.com>
2024-09-07 21:52:59 +0530

    updated

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser,logoutUser,refreshAccessToken,registerUser,changeCurrentPassword } from "../controllers/user.controller.js";
+import { loginUser,logoutUser,refreshAccessToken,registerUser,changeCurrentPassword, getAllUsers } from "../controllers/user.controller.js";
 import { authorizeRoles } from "../middlewares/auth.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router=Router()
@@ -15,5 +15,7 @@ router.route("/logout").post(verifyJWT , logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
 
 router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+
+router.route("/getalluser").post(getAllUsers)
 
 export default router
