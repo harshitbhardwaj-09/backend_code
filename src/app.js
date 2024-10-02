330ccfd89b11e074b27134dae2f512f015365dde
harshitbhardwaj-09 <harshitbhardwaj99999@gmail.com>
2024-10-02 18:02:23 +0530

    added rate limitor

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 import multer from "multer";
+import rateLimit from "express-rate-limit";
 
 const app = express()
 const upload = multer(); // Initialize multer without any storage configuration for handling form-data
@@ -18,7 +19,18 @@ app.use(cookieParser())
 
 import authRouter from './routes/auth.js'
 import apiRouter from './routes/api.js'
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (15 minutes)
+    message: {
+        error: "Too many requests, please try again later."
+    },
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
+// Apply the rate limiter to all requests
+app.use(limiter);
 app.use("/admin",authRouter);
 app.use("/api",apiRouter);
 
